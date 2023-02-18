f987d0f9dd5b7c1429c2a60d9127274561f4d7fd
Simon Ullado <99321661+smnlld@users.noreply.github.com>
2023-02-18 15:10:46 +0800

    idk how to preview the table

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-const Event = ({event}) => {
+const Event = ({ event }) => {
   return (
     <div>
-      <h3>{event.Title}</h3>
-      <p>{event.Description}</p>
-      <p>{event.Status}</p>
+      <table>
+        <tr>
+          <h1>
+          <th>COMPLETED</th>
+          <th>TO DO</th>
+          <th>NEW TASK</th>
+          </h1></tr>
+        <tr>
+          <td>
+            <h3>{event.Title}</h3>
+            <p>{event.Description}</p>
+            <p>{event.Status}</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Hello
+          </td>
+        </tr>
+      </table>
     </div>
   );
 };
