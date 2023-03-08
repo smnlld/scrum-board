524999d33eebd33cc113be15b7b9b25a17d39787
Simon Ullado <99321661+smnlld@users.noreply.github.com>
2023-03-08 20:04:40 +0800

    asdasd

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -25,7 +25,7 @@ export const ToDo = ({ events, statusUpdate, setEvents }) => {
                 PENDING
               </button>
               <button
-              className="bg-green hover:bg-yellow text-white font-bold py-2 px-4 rounded"
+              className="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 rounded"
                 key={event.id}
                 onClick={() => {
                   statusUpdate(event.id, "Completed");
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      'sm': '480px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '547px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '768px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1024px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1680px',
-      // => @media (min-width: 1536px) { ... }
-    },
-    extend: {colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    },
-  },
+    extend: {},
   },
   plugins: [],
 }
